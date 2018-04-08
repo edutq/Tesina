cc41bdfba879c1a1940a56cb128aac8f4ccd975f
edutq <etavarezq@gmail.com>
2018-04-08 12:59:45 -0500

    identifica que hay un cuerpo

--- Tesina/Tesina/MainWindow.xaml.cs
+++ Tesina/Tesina/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ namespace Tesina
     using System.Windows.Media.Imaging;
     using System.Windows.Threading;
     using Microsoft.Kinect;
+    using System.Diagnostics;
 
     /// <summary>
     /// Interaction logic for the MainWindow
@@ -19,6 +20,13 @@ namespace Tesina
 
         private MultiSourceFrameReader frameReader = null;
 
+        private Body[] bodies = null;
+
+        private int activeBodyIndex = 0;
+
+        private BodyFrameReader bodyFrameReader = null;
+
+        private gestureDetector GD = null;
 
         public MainWindow()
         {
@@ -28,9 +36,12 @@ namespace Tesina
 
             this.kinectSensor.Open();
 
+            this.bodyFrameReader = this.kinectSensor.BodyFrameSource.OpenReader();
+
             this.frameReader = this.kinectSensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Body);
             frameReader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
 
+            this.GD = new gestureDetector(kinectSensor, this.Word);
 
         }
 
@@ -38,7 +49,7 @@ namespace Tesina
         {
             // Get a reference to the multi-frame
             var reference = e.FrameReference.AcquireFrame();
-
+            bool dataReceived = false;
             // Open color frame
             using (var frame = reference.ColorFrameReference.AcquireFrame())
             {
@@ -53,10 +64,91 @@ namespace Tesina
             {
                 if (frame != null)
                 {
-                    
+                    //this.Word.Text = "Veo el cuerpo";
+                    using (var bodyFrame = this.bodyFrameReader.AcquireLatestFrame())
+                    {
+                        if (bodyFrame != null)
+                        {
+                            if (bodyFrame != null)
+                            {
+                                if (this.bodies == null)
+                                {
+                                    // creates an array of 6 bodies, which is the max number of bodies that Kinect can track simultaneously
+                                    this.bodies = new Body[bodyFrame.BodyCount];
+                                }
+
+                                // The first time GetAndRefreshBodyData is called, Kinect will allocate each Body in the array.
+                                // As long as those body objects are not disposed and not set to null in the array,
+                                // those body objects will be re-used.
+                                bodyFrame.GetAndRefreshBodyData(this.bodies);
+
+                                if (!this.bodies[this.activeBodyIndex].IsTracked)
+                                {
+                                    // we lost tracking of the active body, so update to the first tracked body in the array
+                                    int bodyIndex = this.GetActiveBodyIndex();
+
+                                    if (bodyIndex > 0)
+                                    {
+                                        this.activeBodyIndex = bodyIndex;
+                                    }
+                                }
+
+                                dataReceived = true;
+                            }
+
+                            if (dataReceived)
+                            {
+                                Body activeBody = this.bodies[this.activeBodyIndex];
+
+                                // visualize the new gesture data
+                                if (activeBody.TrackingId != this.GD.TrackingId)
+                                {
+                                    // if the tracking ID changed, update the detector with the new value
+                                    this.GD.TrackingId = activeBody.TrackingId;
+                                }
+
+                                if (this.GD.TrackingId == 0)
+                                {
+                                    // the active body is not tracked, pause the detector and update the UI
+                                    this.GD.IsPaused = true;
+                                }
+                                else
+                                {
+                                    // the active body is tracked, unpause the detector
+                                    this.GD.IsPaused = false;
+
+                                    // steering gestures are only valid when the active body's hand state is 'closed'
+                                    // update the detector with the latest hand state
+
+
+                                    // get the latest gesture frame from the sensor and updates the UI with the results
+                                    this.GD.UpdateGestureData();
+                                }
+                            }
+                        }
+                    }
+                        
                 }
             }
 
+        }
+
+        private int GetActiveBodyIndex()
+        {
+            int activeBodyIndex = -1;
+            int maxBodies = this.kinectSensor.BodyFrameSource.BodyCount;
+
+            for (int i = 0; i < maxBodies; ++i)
+            {
+                // find the first tracked body and verify it has hands tracking enabled (by default, Kinect will only track handstate for 2 people)
+                if (this.bodies[i].IsTracked && (this.bodies[i].HandRightState != HandState.NotTracked || this.bodies[i].HandLeftState != HandState.NotTracked))
+                {
+                    activeBodyIndex = i;
+                    break;
+                }
+            }
+
+            return activeBodyIndex;
         }
 
         private ImageSource ToBitmap(ColorFrame frame)
---
+++ Tesina/Tesina/gestureDetector.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+using Microsoft.Kinect.VisualGestureBuilder;
+using System.Diagnostics;
+using System.Windows.Controls;
+
+namespace Tesina
+{
+    public class gestureDetector
+    {
+
+        private readonly string gestureDatabase = @"Database\continuo.gbd";
+
+        private readonly string abrir_inico = "abrir_inicio";
+
+        private readonly string abrir_final = "abrir_final";
+
+        private readonly string abrirProgress = "abrirProgress";
+
+        private bool abrirIniciad = false;
+
+        private bool abrirFinal = false;
+
+        private float abrirProgreso = 0.0f;
+
+        private TextBlock word;
+        /// <summary> Gesture frame source which should be tied to a body tracking ID </summary>
+        private VisualGestureBuilderFrameSource vgbFrameSource = null;
+
+        /// <summary> Gesture frame reader which will handle gesture events coming from the sensor </summary>
+        private VisualGestureBuilderFrameReader vgbFrameReader = null;
+
+        public gestureDetector(KinectSensor kinectSensor, TextBlock palabra)
+        {
+            if (kinectSensor == null)
+            {
+                throw new ArgumentNullException("kinectSensor");
+            }
+
+            this.word = palabra;
+
+            // create the vgb source. The associated body tracking ID will be set when a valid body frame arrives from the sensor.
+            this.vgbFrameSource = new VisualGestureBuilderFrameSource(kinectSensor, 0);
+
+            // open the reader for the vgb frames
+            this.vgbFrameReader = this.vgbFrameSource.OpenReader();
+            if (this.vgbFrameReader != null)
+            {
+                this.vgbFrameReader.IsPaused = true;
+            }
+
+            // load all gestures from the gesture database
+            using (var database = new VisualGestureBuilderDatabase(this.gestureDatabase))
+            {
+                this.vgbFrameSource.AddGestures(database.AvailableGestures);
+            }
+
+            // disable the set of gestures which determine the 'keep straight' behavior, we will use hand state instead
+
+        }
+
+        public ulong TrackingId
+        {
+            get
+            {
+                return this.vgbFrameSource.TrackingId;
+            }
+
+            set
+            {
+                if (this.vgbFrameSource.TrackingId != value)
+                {
+                    this.vgbFrameSource.TrackingId = value;
+                }
+            }
+        }
+
+        public bool IsPaused
+        {
+            get
+            {
+                return this.vgbFrameReader.IsPaused;
+            }
+
+            set
+            {
+                if (this.vgbFrameReader.IsPaused != value)
+                {
+                    this.vgbFrameReader.IsPaused = value;
+                }
+            }
+        }
+
+        public void UpdateGestureData()
+        {
+            using (var frame = this.vgbFrameReader.CalculateAndAcquireLatestFrame())
+            {
+                
+                if (frame != null)
+                {
+                    //this.word.Text = "AQUI ANDO";
+                    // get all discrete and continuous gesture results that arrived with the latest frame
+                    var discreteResults = frame.DiscreteGestureResults;
+                    var continuousResults = frame.ContinuousGestureResults;
+
+                    if (discreteResults != null)
+                    {
+                        foreach (var gesture in this.vgbFrameSource.Gestures)
+                        {
+                            
+                            if (gesture.GestureType == GestureType.Discrete)
+                            {
+                                DiscreteGestureResult result = null;
+                                discreteResults.TryGetValue(gesture, out result);
+
+                                if (result != null)
+                                {
+                                    if (gesture.Name.Equals(abrir_inico))
+                                    {
+                                        //this.word.Text = "inicio abrir";
+                                        abrirIniciad = result.Detected;
+                                    }
+                                    else if (gesture.Name.Equals(abrir_final))
+                                    {
+                                        //this.word.Text = "final abrir";
+                                        abrirFinal = result.Detected;
+                                    }
+                                }
+                            }
+
+                            if (continuousResults != null)
+                            {
+                                if (gesture.Name.Equals(this.abrirProgress) && gesture.GestureType == GestureType.Continuous)
+                                {
+                                    ContinuousGestureResult result = null;
+                                    continuousResults.TryGetValue(gesture, out result);
+
+                                    if (result != null)
+                                    {
+                                        abrirProgreso = result.Progress;
+                                    }
+                                }
+                            }
+                        }
+                        if (abrirIniciad && abrirFinal && abrirProgreso > 0.8f)
+                        {
+                            this.word.Text = "DIJISTE ABRIR";
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
